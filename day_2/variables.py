401681cc812a102dc791f78364d75ae28708be2a
saanvi singhal <147823776+saanvisinghal24@users.noreply.github.com>
2025-12-14 19:00:36 +0530

    some simple codes for understanding

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,1 +1,12 @@
-
+#Day 2: 30 Days of python programming
+first_name = 'saanvi'
+last_name = 'singhal'
+full_name = 'saanvi singhal'
+country = 'india'
+city = 'saharanpur'
+age = 21
+year = 2025
+is_married = False
+is_true = True
+is_light_on = True
+name,section,college,dream_country = 'saanvi singhal','E','ABVGIET','Germany'
