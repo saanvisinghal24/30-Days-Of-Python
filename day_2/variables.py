17dc2dab4a07c3d73c785586504bbc28a1d407b9
saanvi singhal <147823776+saanvisinghal24@users.noreply.github.com>
2025-12-22 21:53:55 +0530

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -10,3 +10,51 @@ is_married = False
 is_true = True
 is_light_on = True
 name,section,college,dream_country = 'saanvi singhal','E','ABVGIET','Germany'
+#type function
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+#len() function
+length_first_name = len(first_name)
+length_last_name = len(last_name)
+print('length of first name is:',length_first_name)
+if(length_first_name>length_last_name):
+  print('len_first_name is greater')
+if(length_first_name<length_last_name):
+  print('len_first_name is smaller')
+else:
+  print('both are equal')
+
+num_one = 5
+num_two = 4
+total = num_one+num_two
+print(total)
+diff = num_one-num_two
+print(diff)
+product = num_one*num_two
+print(product)
+division = num_one/num_two
+print(division)
+remainder = nume_one%num_two
+print(remainder)
+exp = num_one**num_two
+print(exp)
+floor_divison = nume_one//num_two
+print(floor_divison)
+
+#circle
+radius=30
+area_of_circle = pi*radius*radius
+circum_of_circle = 2*pi*radius
+R = input(int('Radius:'))
+area = pi*R*R
+
+first_name = input("Enter your first name:")
