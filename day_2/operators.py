1a884f511711dfb73929e854a63281ea1dc790d4
saanvi singhal <147823776+saanvisinghal24@users.noreply.github.com>
2025-12-23 18:19:10 +0530

    Update operators.py

--- day_2/operators.py
+++ day_2/operators.py
@@ -1,3 +1,3 @@
-age = int(input('Enter age:')
-height = float(input('Enter height:')
-number = complex(input('Enter a complex number:')               
+age = int(input('Enter age:'))
+height = float(input('Enter height:'))
+number = complex(input('Enter a complex number:'))               
