0fb8efb331f5429c85c3a16297675782b2c5ff34
saanvi singhal <147823776+saanvisinghal24@users.noreply.github.com>
2025-12-23 18:39:50 +0530

    Update operators.py

--- day_2/operators.py
+++ day_2/operators.py
@@ -1,3 +1,15 @@
 age = int(input('Enter age:'))
 height = float(input('Enter height:'))
 number = complex(input('Enter a complex number:'))               
+ # area of triangle
+base = int(input('Enter base:'))
+height = int(input('Enter height:'))
+Area_of_triangle = 0.5*base*height
+print('Area of triangle is:',Area_of_triangle)
+
+#perimeter of triangle
+a = input('Side a:')
+b = input('Side b:')
+c = input('Side c:')
+Perimeter = a+b+c
+print('Perimeter of triangle:',Perimeter)
